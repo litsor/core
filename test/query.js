03a588f05a4c1a5181304905d7b6cae547cd9caf
Maurits Lawende <maurits@immer.im>
2016-10-15 20:35:46 +0200

    Extend testcases

--- test/query.js
+++ test/query.js
@@ -32,4 +32,177 @@ describe('Query', function() {
       expect(error.message).to.match(/^Query error: /);
     });
   });
+  
+  it('can create data with strings', function() {
+    let query = `{
+      createPost(teststring:"This is a test") {
+        id
+      }
+    }`;
+    return storage.query(query).then((result) => {
+      expect(result.createPost).to.have.property('id');
+      temporary = {id: result.createPost.id};
+    });
+  });
+  
+  it('can get data with strings', function() {
+    let query = `{
+      readPost(id:?) {
+        teststring
+      }
+    }`;
+    let id = temporary.id;
+    return storage.query(query, [id]).then((result) => {
+      expect(result.readPost).to.have.property('teststring', 'This is a test');
+    });
+  });
+  
+  it('can create data with integers', function() {
+    let query = `{
+      createPost(testint:123) {
+        id
+      }
+    }`;
+    return storage.query(query).then((result) => {
+      expect(result.createPost).to.have.property('id');
+      temporary = {id: result.createPost.id};
+    });
+  });
+  
+  it('can get data with integers', function() {
+    let query = `{
+      readPost(id:?) {
+        testint
+      }
+    }`;
+    let id = temporary.id;
+    return storage.query(query, [id]).then((result) => {
+      expect(result.readPost).to.have.property('testint', 123);
+    });
+  });
+  
+  it('can create data with floats', function() {
+    let query = `{
+      createPost(testfloat:1.23) {
+        id
+      }
+    }`;
+    return storage.query(query).then((result) => {
+      expect(result.createPost).to.have.property('id');
+      temporary = {id: result.createPost.id};
+    });
+  });
+  
+  it('can get data with floats', function() {
+    let query = `{
+      readPost(id:?) {
+        testfloat
+      }
+    }`;
+    let id = temporary.id;
+    return storage.query(query, [id]).then((result) => {
+      expect(result.readPost).to.have.property('testfloat', 1.23);
+    });
+  });
+  
+  it('can create data with objects', function() {
+    let query = `{
+      createPost(testobject:{foo:"bar"}) {
+        id
+      }
+    }`;
+    return storage.query(query).then((result) => {
+      expect(result.createPost).to.have.property('id');
+      temporary = {id: result.createPost.id};
+    });
+  });
+  
+  it('can get data with objects', function() {
+    let query = `{
+      readPost(id:?) {
+        testobject
+      }
+    }`;
+    let id = temporary.id;
+    return storage.query(query, [id]).then((result) => {
+      expect(result.readPost).to.have.property('testobject');
+      expect(result.readPost.testobject).to.deep.equal({foo: 'bar'});
+    });
+  });
+  
+  it('can create data with lists', function() {
+    let query = `{
+      createPost(testlist:[{},{}]) {
+        id
+      }
+    }`;
+    return storage.query(query).then((result) => {
+      expect(result.createPost).to.have.property('id');
+      temporary = {id: result.createPost.id};
+    });
+  });
+  
+  it('can get data with lists', function() {
+    let query = `{
+      readPost(id:?) {
+        testlist
+      }
+    }`;
+    let id = temporary.id;
+    return storage.query(query, [id]).then((result) => {
+      expect(result.readPost).to.have.property('testlist');
+      expect(result.readPost.testlist).to.deep.equal([{},{}]);
+    });
+  });
+  
+  it('can create data with booleans', function() {
+    let query = `{
+      createPost(testboolean:true) {
+        id
+      }
+    }`;
+    return storage.query(query).then((result) => {
+      expect(result.createPost).to.have.property('id');
+      temporary = {id: result.createPost.id};
+    });
+  });
+  
+  it('can get data with booleans', function() {
+    let query = `{
+      readPost(id:?) {
+        testboolean
+      }
+    }`;
+    let id = temporary.id;
+    return storage.query(query, [id]).then((result) => {
+      expect(result.readPost).to.have.property('testboolean');
+      expect(result.readPost.testboolean).to.equal(true);
+    });
+  });
+    
+  it('allows us to omit "read" for read operations', function() {
+    let query = `{
+      Post(id:?) {
+        id
+      }
+    }`;
+    let id = temporary.id;
+    return storage.query(query, [id]).then((result) => {
+      expect(result).to.have.property('Post');
+      expect(result.Post).to.have.property('id');
+    });
+  });
+
+  it('allows using aliases', function() {
+    let query = `{
+      p: Post(id:?) {
+        id
+      }
+    }`;
+    let id = temporary.id;
+    return storage.query(query, [id]).then((result) => {
+      expect(result).to.have.property('p');
+      expect(result.p).to.have.property('id');
+    });
+  });
 });
