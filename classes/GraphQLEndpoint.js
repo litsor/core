e30703b1612e909bbe3decfc4fd17fc36980875c
Maurits Lawende <mauritsl@users.noreply.github.com>
2016-11-13 16:47:00 +0100

    Use ES6 style callbacks for GraphQL endpoint

--- classes/GraphQLEndpoint.js
+++ classes/GraphQLEndpoint.js
@@ -21,29 +21,28 @@ class GraphQLEndpoint {
     this.app = app;
     this.storage = storage;
 
-    const self = this;
-    app.process('GET /graphql', function(context) {
-      const query = this.getQuery('q', 'string', '{}');
-      return self.storage.query(query, context).catch(error => {
+    app.process('GET /graphql', (request, context) => {
+      const query = request.getQuery('q', 'string', '{}');
+      return this.storage.query(query, context).catch(error => {
         if (error.message === 'Query error: Permission denied') {
-          this.status = 403;
+          request.status = 403;
           return {errors:[{message:'Permission denied'}]};
         }
         throw error;
       });
     });
-    app.process('POST /graphql', function(context) {
-      const query = this.body.query;
-      let args = this.body.arguments;
+    app.process('POST /graphql', (request, context) => {
+      const query = request.body.query;
+      let args = request.body.arguments;
       if (typeof query !== 'string') {
         throw new Error('Query missing or invalid.');
       }
       if (!(args instanceof Array)) {
         args = [];
       }
-      return self.storage.query(query, context, args).catch(error => {
+      return this.storage.query(query, context, args).catch(error => {
         if (error.message === 'Query error: Permission denied') {
-          this.status = 403;
+          request.status = 403;
           return {errors:[{message:'Permission denied'}]};
         }
         throw error;
