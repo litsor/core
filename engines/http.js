a8c841e341fc928939271aada68a1faee6ecb053
Maurits Lawende <mauritsl@users.noreply.github.com>
2017-06-26 20:28:42 +0200

    Fix JSON detection in HTTP engine

--- engines/http.js
+++ engines/http.js
@@ -162,7 +162,8 @@ class Http extends Model {
         let response;
         return fetch(uri).then(_response => {
           response = _response;
-          if (response.headers.get('Content-Type') === 'application/json') {
+          const mime = response.headers.get('Content-Type').replace(/;.+$/, '');
+          if (mime === 'application/json') {
             return response.json();
           }
           return response.text();
