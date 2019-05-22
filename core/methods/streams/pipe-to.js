fea0b975682304881739aff5dda906bf1123c8b4
Maurits Lawende <mauritsl@users.noreply.github.com>
2019-05-23 00:33:41 +0200

    fix(streams): wait for stream to end in pipeTo method

--- core/methods/streams/pipe-to.js
+++ core/methods/streams/pipe-to.js
@@ -12,7 +12,20 @@ module.exports = {
   binary: async (fromId, toId, {Streams}) => {
     const from = Streams.getStream(fromId);
     const to = Streams.getStream(toId);
+
+    let resolve;
+    let reject;
+    const defer = new Promise((_resolve, _reject) => {
+      resolve = _resolve;
+      reject = _reject;
+    });
+
     from.pipe(to);
+    from.on('finish', resolve);
+    from.on('close', resolve);
+    from.on('error', reject);
+    await defer;
+
     return null;
   }
 };
